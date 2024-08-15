87ade9c69058833fd773ef3fdb0f2569cd340d86
azacdev <azacdev@gmail.com>
2024-08-15 01:42:01 +0100

    chore: change frontend api proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        // target: "http://localhost:5000",
+        target: "https://azacdev-chat-app-backend.vercel.app",
       },
     },
   },
