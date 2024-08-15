3d4f68e6cc2cf55b534206529f7bec8e302bfc16
azacdev <azacdev@gmail.com>
2024-08-15 03:24:24 +0100

    fix

--- backend/index.ts
+++ backend/index.ts
@@ -23,16 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", usersRoutes);
 
-app.use(express.static(path.join(dirname, "../frontend/dist")));
+app.use(express.static(path.join(dirname, "/frontend/dist")));
 
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(dirname, "../frontend", "dist", "index.html"));
+  res.sendFile(path.join(dirname, "frontend", "dist", "index.html"));
 });
-
-console.log(
-  "File Path",
-  path.join(dirname, "../frontend", "dist", "index.html")
-);
 
 server.listen(PORT, () => {
   connectToDB();
