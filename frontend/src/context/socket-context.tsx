d091da1c915b6018dfe2b551c54061a8e980d342
azacdev <azacdev@gmail.com>
2024-08-15 02:41:39 +0100

    fix

--- frontend/src/context/socket-context.tsx
+++ frontend/src/context/socket-context.tsx
@@ -24,7 +24,7 @@ export const SocketContextProvider = ({
 
   useEffect(() => {
     if (authUser) {
-      const socketInstance = io("https://chat-app-pf41.onrender.com", {
+      const socketInstance = io("https://chat-app-ng.onrender.com", {
         query: { userId: authUser._id },
       });
 
